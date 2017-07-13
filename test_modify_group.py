707860c0ec8046aed391607ec5bdc5597ada180b
tshylina <t.shylina@gmail.com>
2017-07-13 21:29:22 +0300

    new5

--- test_modify_group.py
+++ test_modify_group.py
@@ -1,1 +1,6 @@
 def.test_modify_group(app, init_login, init_group):
+    data_for_changing = Group()
+    app.open_group_page()
+    app.modify_group_page()
+    assert "..." in app.message()
+    app.return_to_page()
