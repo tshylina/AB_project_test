9d767541b5f8eb698b2106bad62d8aab370663c0
tshylina <t.shylina@gmail.com>
2017-07-17 19:51:03 +0300

    new2

--- data/groups_data.py
+++ data/groups_data.py
@@ -3,12 +3,12 @@ from models.group import Group
 
 with open("groups.json", encoding='utf8') as f:
     group_data = json.load(f)
-    groups_list = [Group(name=data["name"], header=data["header"], footer=data["footer"])
-                    for data in group_data]
+    groups_list = [Group(**data) for data in group_data]
+
     # groups_list = []
     # for data in group_data:
     #     g = Group(name=data["name"], header=data["header"], footer=data["footer"])
     #     groups_list.append(g)
 
     # print(groups_list)
-        print(groups_list)
+print(groups_list)
