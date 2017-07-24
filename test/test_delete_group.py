a999aad37461c078ea623f7fbde529977f1d2913
tshylina <t.shylina@gmail.com>
2017-07-24 19:53:26 +0300

    new1

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -1,9 +1,13 @@
 import random
 
-def test_delete_group(app,init_login, init_groups):
+def test_delete_group(app,init_login, init_groups, db):
+    old_group_list = db.get_group_list()
     index =random.randrange(app.group.count())
-
     app.open_group_page()
     app.group.delete(index)
     assert "Group has been removed." in app.message()
     app.return_to_group_page()
+    new_group_list = db.get_group_list()
+    assert len(old_group_list) - 1 == len(new_group_list)
+    old_group_list.pop(index)
+    assert old_group_list == new_group_list
