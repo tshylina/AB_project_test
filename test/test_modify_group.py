590004ece8735422e5a13f2a88792034824933f5
tshylina <t.shylina@gmail.com>
2017-07-26 20:13:42 +0300

    new2

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,4 +1,4 @@
-def.test_modify_group(app, init_login, init_group):
+def test_modify_group(app, init_login, init_group):
     data_for_changing = Group()
     app.open_group_page()
     app.modify_group_page()
