dc69e747a0e898afa061cea637b530e169c306ef
tshylina <t.shylina@gmail.com>
2017-07-26 21:09:47 +0300

    new4

--- test/test_group_create4.py
+++ test/test_group_create4.py
@@ -10,7 +10,7 @@ def test_group_create(app, init_login, group, db):
     # Verify group page
     with pytest.allure.step("THEN the success message appears"):
         assert "A new group has been entered into the address book." in app.message()
-    app.return_to_group_page()
+        app.return_to_group_page()
     with pytest.allure.step("THEN the new group list is equal to the old list with new added group"):
         new_group_list = db.get_group_list()
         assert len (old_group_list) +1 == len (new_group_list)
